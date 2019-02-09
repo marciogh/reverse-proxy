480c1f48850ae1e92dbdb4aa209d149e4f843c98
mghiraldelli <mghiraldelli@atlassian.com>
2019-02-09 12:43:43 +1100

    redis

--- main.go
+++ main.go
@@ -3,42 +3,49 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"github.com/go-redis/redis"
 	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
 	"strings"
-	"sync"
 	"time"
 )
 
 type rateLimitTransport struct {
-	limit      int
-	countMutex sync.RWMutex
-	countMap   map[string]int
-}
-
-func (r *rateLimitTransport) count(remoteAddr string) int {
-	r.countMutex.Lock()
-	defer r.countMutex.Unlock()
-	r.countMap[remoteAddr]++
-	return r.countMap[remoteAddr]
+	limit int64
+	interval time.Duration
+	redisClient *redis.Client
 }
 
 func (t *rateLimitTransport) RoundTrip(req *http.Request) (*http.Response, error) {
 	ip := strings.Split(req.RemoteAddr, ":")[0]
-	if t.count(ip) > t.limit {
+	count, _ := t.redisClient.Eval(`local v=redis.call("incrby",KEYS[1],1) if v==1 then redis.call("expire",KEYS[1],5) end return v`, []string{ip}).Result()
+	if  count.(int64) > t.limit {
 		return &http.Response{
 			StatusCode: 503,
-			Body: ioutil.NopCloser(bytes.NewBufferString("Rate limit exceeded")),
+			Body: ioutil.NopCloser(bytes.NewBufferString("rate limit exceeded")),
 		}, nil
 	}
 	return http.DefaultTransport.RoundTrip(req)
 }
 
+func buildRedisClient() *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+	client.FlushAll()
+	return client
+}
+
 func main() {
 	var transport rateLimitTransport
+	transport.redisClient = buildRedisClient()
+	transport.interval = 5 * time.Second
+	transport.limit = 3
 	var reverseProxy = &httputil.ReverseProxy{
 		Director: func(req *http.Request) {
 			req.URL, _ = url.Parse(os.Getenv("TARGET_URL"))
@@ -46,16 +53,9 @@ func main() {
 		},
 		Transport: &transport,
 	}
-	transport.limit = 3
 	http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
 		reverseProxy.ServeHTTP(w, r)
 	})
-	go func() {
-		for {
-			transport.countMap = make(map[string]int)
-			time.Sleep(5 * time.Second)
-		}
-	}()
-	fmt.Println("Reverse Proxy listening on port 8080")
+	fmt.Println("listening on port 8080")
 	http.ListenAndServe(":8080", nil)
 }
